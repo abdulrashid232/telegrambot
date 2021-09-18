26552ca9972318c8116332b7ead03c42250f186a
iamabdel <abdelrash232@gmail.com>
2021-09-18 14:02:45 +0000

    added two new fucntions

--- mov.py
+++ mov.py
@@ -5,7 +5,7 @@ from api import Token
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Hi! Am a movie bot , type /help of more infor")
 
-
+ 
 def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("This is a movie rating bot, But the bot isn't fully ready")
 
@@ -13,6 +13,12 @@ def help_command(update: Update, context: CallbackContext) -> None:
 def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
 
+
+def search(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("This is a command used to search for movies. NB: still in working progress.......")
+
+def rating(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text("This a command use to get movie ratings from imdb. NB: still in working progress.......")
 
 def main() -> None:
     updater = Updater(Token)
@@ -24,6 +30,8 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("help", help_command))
 
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(CommandHandler("search", search))
+    dispatcher.add_handler(CommandHandler("rating", rating))
 
     updater.start_polling()
 
