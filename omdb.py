996ec1a01358e7209afc853fd3b92175914fa390
iamabdel <abdelrash232@gmail.com>
2021-09-30 08:14:12 +0000

    added the movie functions

---
+++ omdb.py
@@ -0,0 +1,22 @@
+import os 
+import requests
+
+def movie_infor(movieTitle):
+    api_key = os.getenv("Movie_api")
+    url = "http://www.omdbapi.com/?apikey=" + api_key
+    t = "&t=" + movieTitle
+    response = requests.get(url + t)
+
+    if response.get("Response") != "True":
+        return None
+
+    movie_info = {}
+    movie_info["title"] = response.get("Title")
+    movie_info["year"] = response.get("Year")
+    movie_info["plot"] = response.get("Plot")
+    movie_info["actors"] = response.get("Actors")
+    movie_info["ratings"] = response.get("Ratings")
+    movie_info["imdb_ratings"] = response.get("imdb_rating") = response.get("imdbRating")
+
+
+    return movie_info
